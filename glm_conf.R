70857d8852a2babbf67c11ac930d1a4bd5b666b0
Sara Larcher <slarcher31770@gmail.com>
2024-10-10 17:40:16 +0200

    Modele mixte(ridge-lasso)

--- glm_conf.R
+++ glm_conf.R
@@ -1,5 +1,6 @@
 #Package
 require(caret)
+require(glmnet)
 
 #Chargement
 load("dta.RData")
@@ -13,10 +14,29 @@ test <- dta[-train_indices, ]
 
 train_control <- trainControl(method = "LGOCV", number = 10, p=0.7) 
 
-#Modele 
+#GLM 
 glm_model <- train(Depression_level ~ ., data = train, 
                    method = "glm", 
                    family = gaussian, 
                    trControl = train_control)
 glm_model
-predictions <- predict(glm_model, newdata = test)
+predictions_glm <- predict(glm_model, newdata = test)
+RMSE_glm <- sqrt(mean((predictions_glm - test$Depression_level)^2)) 
+
+#Mixte (lasso-ridge)
+lambda <- seq(0.001, 2, length = 20)
+alpha <- seq (0, 1, length=10)
+mixte_model <- train(Depression_level ~ ., data = train, 
+                     method = "glmnet", 
+                     trControl = train_control,
+                     tuneGrid = expand.grid(alpha = alpha,  
+                                            lambda = lambda))  
+
+
+
+mixte_model$bestTune
+predictions_mixte <- predict(mixte_model, newdata = test)
+RMSE_mixte <- sqrt(mean((predictions_mixte - test$Depression_level)^2)) 
+res <- mixte_model$results
+plot(res[,2], res[,3])
+
