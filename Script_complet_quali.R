2aaf6d7145fc5f8ed346ee35c726d7c1d96c5f22
Nelhe <nel.herve@gmail.com>
2024-10-23 17:12:16 +0200

    Boucles réparées

--- Script_complet_quali.R
+++ Script_complet_quali.R
@@ -37,89 +37,68 @@ source("Scripts exploratoires/Func_perf.R")
 # k plus proches voisins
 library(kknn)
 
-tune_knn <- expand.grid(k = 1:50,
+tune_knn <- expand.grid(k = seq(25, 75, by = 5),
                         s = seq(0.2, 0.5, by = 0.05))
 
-k_opti <- rep(0, 10)
-seuil_opti <- rep(0, 10)
+f1_score_knn <- rep(0, nrow(tune_knn))
 
-for (k in 1:10){
-  train <- dta_train[-segs[[k]],]
-  valid <- dta_train[segs[[k]],]
+for (i in 1:nrow(tune_knn)){
+  preds <- rep(0, nrow(dta_train))
   
-  f1_scores <- rep(0, nrow(tune_knn))
-  
-  for (i in 1:nrow(tune_knn)){
+  for (k in 1:10){
+    train <- dta_train[-segs[[k]],]
+    valid <- dta_train[segs[[k]],]
+    
     mod_knn <- kknn(Depression_severity ~., train = train, test = valid, k = tune_knn[i,1])
     
-    pred_classe <- apply(mod_knn$prob, MARGIN = 1, FUN = function(x, s) return(ifelse(x[2] > s, 1, 0)),
+    preds[segs[[k]]] <- apply(mod_knn$prob, MARGIN = 1, FUN = function(x, s) return(ifelse(x[2] > s, 1, 0)),
                          s = tune_knn[i,2])
-    
-    f1_scores[i] <- f1(as.numeric(valid$Depression_severity) - 1, 
-                       as.numeric(pred_classe))
   }
   
-  k_opti[[k]] <- tune_knn[which(f1_scores == max(f1_scores))[1],1]
-  seuil_opti[[k]] <- tune_knn[which(f1_scores == max(f1_scores))[1],2]
+  f1_score_knn[i] <- f1(as.numeric(dta_train$Depression_severity) - 1,
+                 preds)
   
-  print(paste("kNN fold :", k, "/ 10 - DONE"))
+  print(paste(i, nrow(tune_knn), sep = '/'))
 }
 
-k_opti
-seuil_opti
+perf_knn <- data.frame(Méthode = 'kNN',
+                       tune_knn,
+                       F1_score = f1_score_knn) |>
+  dplyr::arrange(desc(F1_score))
 
-k_knn <- sort(table(k_opti), decreasing = T)[1] |> 
-  names() |>
-  as.numeric()
-s_knn <- sort(table(seuil_opti), decreasing = T)[1] |> 
-  names() |>
-  as.numeric()
-
-
-mod_knn_opti <- kknn::kknn(Depression_severity ~., train = dta_train, test = dta_test, k = k_knn)
-pred_classe_knn <- apply(mod_knn_opti$prob, MARGIN = 1, FUN = function(x) return(ifelse(x[2] > s_knn, 1, 0)))
-
-conf_knn <- table(dta_test$Depression_severity, pred_classe_knn)
-conf_knn
+perf_knn
 
 
 # GLM
 tune_glm <- expand.grid(s = seq(0.05, 0.5, by = 0.05))
 
-seuil_opti <- rep(0, 10)
+f1_score_glm <- rep(0, nrow(tune_glm))
 
-for (k in 1:10){
-  train <- dta_train[-segs[[k]],]
-  valid <- dta_train[segs[[k]],]
-
-  f1_scores <- rep(0, nrow(tune_glm))
+for (i in 1:nrow(tune_glm)){
+  preds <- rep(0, nrow(dta_train))
   
-  for (i in 1:nrow(tune_glm)){
+  for (k in 1:10){
+    train <- dta_train[-segs[[k]],]
+    valid <- dta_train[segs[[k]],]
     
     mod_glm <- glm(Depression_severity ~., data = train, family = 'binomial')
     pred <- predict(mod_glm, newdata = valid, type = 'response')
-    pred_classe <- ifelse(pred >= tune_glm[i,1], 1, 0)
-
-    f1_scores[i] <- f1(as.numeric(valid$Depression_severity) - 1, 
-                       as.numeric(pred_classe))
+    preds[segs[[k]]] <- ifelse(pred >= tune_glm[i,1], 1, 0)
   }
   
-  seuil_opti[[k]] <- tune_glm[which(f1_scores == max(f1_scores))[1],1]
-  print(paste("GLM fold :", k, "/ 10 - DONE"))
+  f1_score_glm[i] <- f1(as.numeric(dta_train$Depression_severity) - 1,
+                    preds)
+  
+  print(paste(i, nrow(tune_glm), sep = '/'))
 }
 
-seuil_opti
+perf_glm <- data.frame(Méthode = 'GLM',
+                       tune_glm,
+                       F1_score = f1_score_glm) |>
+  dplyr::arrange(desc(F1_score))
 
-s_glm <- sort(table(seuil_opti), decreasing = T)[1] |> 
-  names() |>
-  as.numeric()
+perf_glm
 
-mod_glm_opti <- glm(Depression_severity ~., data = dta_train, family = 'binomial')
-pred_glm <- predict(mod_glm_opti, newdata = dta_test, type = 'response')
-pred_classe_glm <- ifelse(pred_glm >= s_glm, 1, 0)
-
-conf_glm <- table(dta_test$Depression_severity, pred_classe_glm)
-conf_glm
 
 # GLM net
 library(glmnet)
@@ -128,58 +107,35 @@ tune_glmnet <- expand.grid(alp = seq(0, 1, length = 5),
                            lb = seq(1e-4, 1e-1, length = 5),
                            s = seq(0.05, 0.5, by = 0.05))
 
-alpha_opti <- rep(0, 10)
-lambda_opti <- rep(0, 10)
-seuil_opti <- rep(0, 10)
+f1_score_glmnet <- rep(0, nrow(tune_glmnet))
 
-for (k in 1:10){
-  train <- dta_train[-segs[[k]],]
-  valid <- dta_train[segs[[k]],]
+for (i in 1:nrow(tune_glmnet)){
+  preds <- rep(0, nrow(dta_train))
   
-  f1_scores <- rep(0, nrow(tune_glmnet))
-  
-  for (i in 1:nrow(tune_glmnet)){
+  for (k in 1:10){
+    train <- dta_train[-segs[[k]],]
+    valid <- dta_train[segs[[k]],]
+    
     mod_glmnet <- glmnet(x = as.matrix(train[-8]), y = train$Depression_severity, 
                          family = 'binomial',
                          alpha = tune_glmnet[i,1], lambda = tune_glmnet[i,2])
     
     pred <- predict(mod_glmnet, newx = as.matrix(valid[-8]), type = 'response')
-    pred_classe <- ifelse(pred > tune_glmnet[i,3], 1, 0)
-    
-    f1_scores[i] <- f1(as.numeric(valid$Depression_severity) - 1, 
-                       as.numeric(pred_classe))
+    preds[segs[[k]]] <- ifelse(pred > tune_glmnet[i,3], 1, 0)
   }
   
-  alpha_opti[[k]] <- tune_glmnet[which(f1_scores == max(f1_scores))[1],1]
-  lambda_opti[[k]] <- tune_glmnet[which(f1_scores == max(f1_scores))[1],2]
-  seuil_opti[[k]] <- tune_glmnet[which(f1_scores == max(f1_scores))[1],3]
+  f1_score_glmnet[i] <- f1(as.numeric(dta_train$Depression_severity) - 1,
+                        preds)
   
-  print(paste("GLM net fold :", k, "/ 10 - DONE"))
+  print(paste(i, nrow(tune_glmnet), sep = '/'))
 }
 
-alpha_opti
-lambda_opti
-seuil_opti
+perf_glmnet <- data.frame(Méthode = 'GLMnet',
+                       tune_glmnet,
+                       F1_score = f1_score_glmnet) |>
+  dplyr::arrange(desc(F1_score))
 
-alpha_glmnet <- sort(table(alpha_opti), decreasing = T)[1] |> 
-  names() |>
-  as.numeric()
-lambda_glmnet <- sort(table(lambda_opti), decreasing = T)[1] |> 
-  names() |>
-  as.numeric()
-s_glmnet <- sort(table(seuil_opti), decreasing = T)[1] |> 
-  names() |>
-  as.numeric()
-
-mod_glmnet_opti <- glmnet(x = as.matrix(dta_train[-8]), y = dta_train$Depression_severity, 
-                          family = 'binomial',
-                          alpha = alpha_glmnet, lambda = lambda_glmnet)
-
-pred_glmnet <- predict(mod_glmnet_opti, newx = as.matrix(dta_test[-8]), type = 'response')
-pred_classe_glmnet <- ifelse(pred_glmnet > s_glmnet, 1, 0)
-
-conf_glmnet <- table(dta_test$Depression_severity, pred_classe_glmnet)
-conf_glmnet
+perf_glmnet
 
 
 # SVM
@@ -188,52 +144,35 @@ library(e1071)
 tune_svm <- expand.grid(C = seq(0.1, 2, length = 5),
                         s = seq(0.05, 0.5, by = 0.05))
 
-C_opti <- rep(0, 10)
-seuil_opti <- rep(0, 10)
+f1_score_svm <- rep(0, nrow(tune_svm))
 
-for (k in 1:10){
-  train <- dta_train[-segs[[k]],]
-  valid <- dta_train[segs[[k]],]
+for (i in 1:nrow(tune_svm)){
+  preds <- rep(0, nrow(dta_train))
   
-  f1_scores <- rep(0, nrow(tune_svm))
-  
-  for (i in 1:nrow(tune_svm)){
+  for (k in 1:10){
+    train <- dta_train[-segs[[k]],]
+    valid <- dta_train[segs[[k]],]
+    
     mod_svm <- svm(Depression_severity ~., data = train, cost = tune_svm[i,1], probability = T)
     
     pred <- predict(mod_svm, newdata = valid, probability = T) |>
       attributes()
-    pred_classe <- apply(pred$probabilities, MARGIN = 1, FUN = function(x, s) return(ifelse(x[1] > s, 1, 0)),
+    preds[segs[[k]]] <- apply(pred$probabilities, MARGIN = 1, FUN = function(x, s) return(ifelse(x[1] > s, 1, 0)),
                          s = tune_svm[i,2])
-    
-    f1_scores[i] <- f1(as.numeric(valid$Depression_severity) - 1, 
-                       as.numeric(pred_classe))
   }
   
-  C_opti[[k]] <- tune_svm[which(f1_scores == max(f1_scores))[1],1]
-  seuil_opti[[k]] <- tune_svm[which(f1_scores == max(f1_scores))[1],2]
+  f1_score_svm[i] <- f1(as.numeric(dta_train$Depression_severity) - 1,
+                           preds)
   
-  print(paste("SVM fold :", k, "/ 10 - DONE"))
+  print(paste(i, nrow(tune_svm), sep = '/'))
 }
 
-C_opti
-seuil_opti
+perf_svm <- data.frame(Méthode = 'SVM',
+                          tune_svm,
+                          F1_score = f1_score_svm) |>
+  dplyr::arrange(desc(F1_score))
 
-C_svm <- sort(table(C_opti), decreasing = T)[1] |> 
-  names() |>
-  as.numeric()
-s_svm <- sort(table(seuil_opti), decreasing = T)[1] |> 
-  names() |>
-  as.numeric()
-
-
-mod_svm_opti <- svm(Depression_severity ~., data = dta_train, cost = C_svm, probability = T)
-pred_svm <- predict(mod_svm_opti, newdata = dta_test, probability = T) |>
-  attributes()
-pred_classe_svm <- apply(pred_svm$probabilities, MARGIN = 1, FUN = function(x, s) return(ifelse(x[2] > s, 1, 0)),
-                     s = s_svm)
-
-conf_svm <- table(dta_test$Depression_severity, pred_classe_svm)
-conf_svm
+perf_svm
 
 
 # Random forest
@@ -243,7 +182,7 @@ tune_rf <- expand.grid(mtry = 2:10,
                       min.node.size = 2:10,
                       s = seq(0.05, 0.5, by = 0.05))
 
-segs <- pls::cvsegments(nrow(dta_train), 10)
+f1_scores_rf <- rep(0, nrow(tune_rf))
   
 for (i in 1:nrow(tune_rf)){
   mod_rf <- ranger(Depression_severity ~., data = dta_train, mtry = tune_rf[i,1], 
@@ -253,23 +192,18 @@ for (i in 1:nrow(tune_rf)){
   pred_classe <- apply(pred, MARGIN = 1, FUN = function(x, s) return(ifelse(x[2] > s, 1, 0)),
                        s = tune_rf[i,3])
   
-  f1_scores[i] <- f1(as.numeric(dta_train$Depression_severity) - 1, 
+  f1_scores_rf[i] <- f1(as.numeric(dta_train$Depression_severity) - 1, 
                      as.numeric(pred_classe))
   
   print(paste("Random forest :", i, "/ 810 - DONE"))
 }
   
-mtry_rf <- tune_rf[which(f1_scores == max(f1_scores))[1],1]
-node_size_rf <- tune_rf[which(f1_scores == max(f1_scores))[1],2]
-s_rf <- tune_rf[which(f1_scores == max(f1_scores))[1],3]
+perf_rf <- data.frame(Méthode = 'Random forest',
+                       tune_rf,
+                       F1_score = f1_scores_rf) |>
+  dplyr::arrange(desc(F1_score))
 
-mod_rf_opti <- ranger(Depression_severity ~., data = dta_train, mtry = mtry_rf,
-                       min.node.size = node_size_rf, probability = T)
-pred_rf <- predict(mod_rf_opti, data = dta_test, type = 'response')
-pred_classe_rf <- apply(pred_rf$predictions, MARGIN = 1, FUN = function(x) return(ifelse(x[2] > s_rf, 1, 0)))
-
-conf_rf <- table(dta_test$Depression_severity, pred_classe_rf)
-conf_rf
+perf_rf
 
 
 ########################################
